d9bb6955c27caf731bf7c46195d04f30fd025a68
Valcinei <valcinei@live.com>
2019-02-16 10:54:33 -0400

    configure autocomplete

---
+++ src/debug.rb
@@ -0,0 +1,18 @@
+# ~/Source/ExampleExtension/src/hello/debug.rb
+module Example::HelloWorld
+
+    # Reload extension by running this method from the Ruby Console:
+    #   Example::HelloWorld.reload
+    def self.reload
+      original_verbose = $VERBOSE
+      $VERBOSE = nil
+      pattern = File.join(__dir__, '**/*.rb')
+      Dir.glob(pattern).each { |file|
+        # Cannot use `Sketchup.load` because its an alias for `Sketchup.require`.
+        load file
+      }.size
+    ensure
+      $VERBOSE = original_verbose
+    end
+  
+  end # module
