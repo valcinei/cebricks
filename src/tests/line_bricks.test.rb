21d30e605ebb7b26c3f188b8f16e9d2692cf0d67
Valcinei Silva <valcinei.silva@fpf.br>
2019-02-19 13:48:43 -0400

    add tests

--- src/tests/line_bricks.test.rb
+++ src/tests/line_bricks.test.rb
@@ -19,18 +19,22 @@ class TestBoxBricks < Test::Unit::TestCase
     line = BricksLine::Wall.new()
     
     assert_equal(true, line.is_diagonal(0, 0, 0, 2, 2, 0))
-  end
+  end 
   
   def test_edge_is_perpendicular_in_y
     line = BricksLine::Wall.new()
     
-    assert_equal(false, line.is_diagonal(0, 0, 0, 0, 2, 0))
+    assert_equal(false, line.is_diagonal(1, 3, 0, 0, 3, 0))
   end
 
   def test_edge_is_perpendicular_in_x
     line = BricksLine::Wall.new()
-    
-    assert_equal(false, line.is_diagonal(0, 0, 0, 2, 0, 0))
+    assert_equal(false, line.is_diagonal(1, 1, 0, 3, 1, 0))
   end
  
+  def test_edge_is_perpendicular_in_random
+    line = BricksLine::Wall.new()
+    assert_equal(true, line.is_diagonal(0, 0, 0, 331.788 , 206.375, 0))
+  end
+  
 end
